22507345a995157738c9dac3215a41e6c7e3275f
EbiggerR <46164655+ebiggerr@users.noreply.github.com>
2022-04-08 19:54:57 +0800

    Basic server
    simple print text on browser

--- server/hello.go
+++ server/hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	 fmt.Println("Hello, World!")
+func main() {
+	handleWebRequests()
+}
+
+func handleWebRequests() {
+	http.HandleFunc("/", homePage)
+	err := http.ListenAndServe(":3001", nil)
+	if err != nil {
+		return
+	}
+}
+
+func homePage(rw http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(rw, "Hey! Welcome.")
+	if err != nil {
+		return
+	}
 }
